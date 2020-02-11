7befb2f8fa81d5ebaf3e3cab4179f7cc791b00a7
elcarvalho <lessa.eric@gmail.com>
2020-02-11 09:21:37 -0200

    feature: cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Mayara Lessa',
-    email: 'lessa.mayara@gmail.com',
-    password_hash: '1234567890',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
