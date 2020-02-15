f9e92e26021a362ea0e1065145dc96700591abc7
elcarvalho <lessa.eric@gmail.com>
2020-02-15 19:16:27 -0200

    chore(envio de email): configurando nodemailer

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -2,6 +2,7 @@ import * as Yup from 'yup';
 import { isBefore, startOfHour, parseISO, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
+import Mail from '../../lib/Mail';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
@@ -107,7 +108,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     if (req.userId !== appointment.user_id) {
       return res.status(401).json({
@@ -126,6 +135,12 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'voce tem um novo cancelamento',
+    });
 
     return res.json(appointment);
   }
