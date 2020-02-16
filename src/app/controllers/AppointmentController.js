eadc58f2aa384c243d8e9f7575e8596e2085356d
elcarvalho <lessa.eric@gmail.com>
2020-02-16 19:22:23 -0300

    chore(envio de email): configurando templates de e-mail

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -115,6 +115,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -139,7 +144,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'voce tem um novo cancelamento',
+      template: 'cancelation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: '',
-  port: ,
-  secure: ,
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
   auth: {
-    user: '',
-    pass: '',
+    user: '153087ebe737cb',
+    pass: 'd1d590729b7564',
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
